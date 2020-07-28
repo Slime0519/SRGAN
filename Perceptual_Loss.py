3a7b5d4e9da0142d9bf8effdd156c0ada3487857
JunmyeongLee_GIST <slime0519@gist.ac.kr>
2020-07-28 15:30:00 +0900

    add patch extraction code

--- Perceptual_Loss.py
+++ Perceptual_Loss.py
@@ -4,14 +4,27 @@ import numpy as np
 import torchvision.models as models
 import matplotlib.pyplot as plt
 from torchsummary import summary
+import os
+from PIL import Image
 
 PATCH_DIR ="Result_image/patchimage"
+def savepatch(DIRPATH, batch,layer):
+
+    DIRPATH_LAYER = os.path.join(DIRPATH,"layer{}".format(layer))
+    if not os.path.isdir(DIRPATH_LAYER):
+        os.mkdir(DIRPATH_LAYER)
+
+    for i,patch in enumerate(batch):
+        plt.imshow(patch)
+        plt.savefig(os.path.join(DIRPATH_LAYER,"{}th_feature.png".format(i,layer)))
 
 class vggloss(nn.Module):
     def __init__(self,truncate_layer=36):
         super(vggloss, self).__init__()
         self.model = models.vgg.vgg19(pretrained=True)
         self.feature_extraction_model = nn.Sequential(*list(self.model.features)[:truncate_layer+1]).eval()
+
+        self.truncated_layer = truncate_layer
 
         for param in self.feature_extraction_model.parameters():
             param.requires_grad = False
@@ -25,15 +38,15 @@ class vggloss(nn.Module):
         if show:
             patch_image_input = np.array(input_extracted_fatch.cpu().detach()).squeeze()
             print(patch_image_input.shape)
-            patch_image_input = np.transpose(patch_image_input,(1,2,0))
-            plt.imshow(patch_image_input[:,:,0])
-            plt.show()
+            savepatch(os.path.join(PATCH_DIR,"input"),patch_image_input,self.truncated_layer)
+          #  plt.imshow(patch_image_input[:,:,0])
+           # plt.show()
 
             patch_image_target = np.array(target_extracted_fatch.cpu().detach()).squeeze()
             print(patch_image_target.shape)
-            patch_image_target = np.transpose(patch_image_target, (1, 2, 0))
-            plt.imshow(patch_image_target[:, :, 0])
-            plt.show()
+            savepatch(os.path.join(PATCH_DIR,"target"), patch_image_target, self.truncated_layer)
+           # plt.imshow(patch_image_target[:, :, 0])
+          #  plt.show()
 
         return perception_loss
     """
